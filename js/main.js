b727cdba323062e984b94ea748eb6bddffe128b6
William Lardner <william.lardner@student.manchester.ac.uk>
2025-01-23 16:05:02 +0000

    Remove jQuery and improve background style/animation
    Previously, the JavaScript relied on jQuery. This has been changed
    to improve maintainability. The tsparticles library is now used
    for a cleaner firefly particles effect.
    
    More configuration options added to main.js and background made
    slightly darker.

--- js/main.js
+++ js/main.js
@@ -1,77 +1,111 @@
-// If you don't want the particles, change the following to false
+// Set to false if you do not want the 'firefly' particles to display in the background
 const doParticles = true;
 
+// If set to true, the playercount banner will display the serverOfflineText (ie "Server isn't online!")
+// If set to false, the playercount banner will hide the playercount (ie "Join other players on <IP>")
+const tellUsersWhenServerIsOffline = true;
+
+// If tellUsersWhenServerIsOffline is set to true, what text should be displayed?
+const serverOfflineText = "Server isn't online!";
+
+// When a user clicks on your server IP, what should it display briefly after the IP has been copied?
+const ipCopiedText = "IP copied!";
 
 
-
-// Do not mess with the rest of this file unless you know what you're doing
-
-const getWidth = () => { // credit to travis on stack overflow
-    return Math.max(
-        document.body.scrollWidth,
-        document.documentElement.scrollWidth,
-        document.body.offsetWidth,
-        document.documentElement.offsetWidth,
-        document.documentElement.clientWidth
-    );
-};
-
+// Particles initialisation
 if (doParticles) {
-    if (getWidth() < 400) $.firefly({
-        minPixel: 1,
-        maxPixel: 2,
-        total: 20
-    });
-    else $.firefly({
-        minPixel: 1,
-        maxPixel: 3,
-        total: 40
-    });
+    (async () => {
+        await loadSlim(tsParticles);
+        await tsParticles.load({
+          id: "tsparticles",
+          options: {
+            fpsLimit: 100,
+            particles: {
+                number: {
+                    value: 100,
+                    density: { enable: true },
+                },
+                color: {
+                    value: ["#ffffff", "#d4f4fc", "#dfebed"],
+                },
+                opacity: {
+                    value: { min: 0.1, max: 0.5 },
+                },
+                size: {
+                    value: { min: 0.7, max: 2 },
+                },
+                move: {
+                    enable: true,
+                    speed: 1.5,
+                    random: false,
+                },
+            }
+          },
+        });
+      })();
 }
 
 // This is for the click to copy
-let t;
-$(document).ready(() => {
-    t = $(".ip").html();
-});
+const ipSpanElement = document.getElementById('ip');
+const originalIPText = ipSpanElement.innerHTML;
 
-$(document).on("click", ".ip", () => {
-    let copy = document.createElement("textarea");
-    copy.style.position = "absolute";
-    copy.style.left = "-99999px";
-    copy.style.top = "0";
-    copy.setAttribute("id", "ta");
-    document.body.appendChild(copy);
-    copy.textContent = t;
-    copy.select();
+ipSpanElement.addEventListener("click", () => {
+    // Create a temporary textarea to copy the IP
+    let tempTextarea = document.createElement("textarea");
+    tempTextarea.style.position = "absolute";
+    tempTextarea.style.left = "-99999px";
+    tempTextarea.style.top = "0";
+    document.body.appendChild(tempTextarea);
+    tempTextarea.textContent = originalIPText;
+    tempTextarea.select();
     document.execCommand("copy");
-    $(".ip").html("<span class='extrapad'>IP copied!</span>");
+    document.body.removeChild(tempTextarea);
+
+    // Temporarily display the IP copied text
+    ipSpanElement.innerHTML = `<span class='extrapad'>${ipCopiedText}</span>`;
     setTimeout(() => {
-        $(".ip").html(t);
-        var copy = document.getElementById("ta");
-        copy.parentNode.removeChild(copy);
+        ipSpanElement.innerHTML = originalIPText;
     }, 800);
 });
 
 // This is to fetch the player count
-$(document).ready(() => {
-    let ip = $(".sip").attr("data-ip");
-    let port = $(".sip").attr("data-port");
-    if (port == "" || port == null) port = "25565";
+const playercountBannerElement = document.getElementById('playercount');
+const playercountSpanElement = document.getElementById('sip');
+
+const initialisePlayercountFetcher = () => {
+    const ip = playercountSpanElement.getAttribute("data-ip");
+    const port = playercountSpanElement.getAttribute("data-port") || "25565";
+
     if (ip == "" || ip == null) return console.error("Error fetching player count - is the IP set correctly in the HTML?");
+    
     updatePlayercount(ip, port);
-    // Updates every minute (not worth changing due to API cache)
-    setInterval(() => {
-        updatePlayercount(ip, port);
-    }, 60000);
-});
+
+    // Update player count every minute (not worth changing due to API cache)
+    setInterval(() => updatePlayercount(ip, port), 60000);
+};
 
 const updatePlayercount = (ip, port) => {
-    $.get(`https://api.bybilly.uk/api/players/${ip}/${port}`, (result) => {
-        if (result.hasOwnProperty('online')) {
-            $(".sip").html(result.online);
+    fetch(`https://api.bybilly.uk/api/players/${ip}/${port}`)
+    .then(res => res.json())
+    .then(data => {
+        if (data.hasOwnProperty('online')) {
+            // Update player count
+            playercountSpanElement.innerHTML = data.online;
+            playercountSpanElement.style.display = 'inline';
         } else {
-            $(".playercount").html("Server isn't online!");
+            // Server is offline - change banner to server offline text or hide player count (see line 6)
+            if (tellUsersWhenServerIsOffline) {
+                playercountBannerElement.innerHTML = serverOfflineText;
+            } else {
+                playercountSpanElement.style.display = 'none';
+            }
         }
+    })
+    .catch(err => {
+        // Hide player count on error
+        console.error(`Error fetching playercount: ${err}`);
+        playercountSpanElement.style.display = 'none';
     });
 };
+
+initialisePlayercountFetcher();
